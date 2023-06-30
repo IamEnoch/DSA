e3dcd17420bde69f3333cbb8a9e89f557bab32f2
IamEnoch <enoch.mwangi@outlook.com>
2023-06-30 12:53:42 +0300

    Rversing a string using a string builder and for loop

---
+++ AlgorithmDrivenStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace AlgorithDrivenString
+{
+    class Program
+    {
+        //Reversing the string using a string builder
+        static string ReverseString(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return name;
+            }
+
+            StringBuilder reverseString = new StringBuilder();
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverseString.Append(name[i]);
+            }
+
+            return reverseString.ToString();
+        }
+        static void Main(String[] args)
+        {
+            string inputName;
+            Console.WriteLine("Enter the word you wish to reverse");
+            inputName = Console.ReadLine();
+
+            Console.WriteLine($"The reverse of {inputName} is: {ReverseString(inputName)}");
+
+        }
+    }
+}
