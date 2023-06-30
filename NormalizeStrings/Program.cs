aa69a938134c96d0b22b6abb12a00e76c3109c9b
IamEnoch <enoch.mwangi@outlook.com>
2023-06-30 11:51:50 +0300

    Added project files

---
+++ NormalizeStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace NormalizeStrings
+{
+    class Program
+    {
+        //Normalize string method 
+        static string NormalizeStringMethod(string name)
+        {
+            //Make the string lowercase, remove whitespaces from start to end of the string and remove ','
+            var normalized = name.ToLower().Trim().Replace(",", "");
+            return normalized;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NormalizeStringMethod(" The name of the stree is called chECK ")); //Expected output: the name of the stree is called check
+
+        }
+    }
+}
---
+++ ValidateStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StringAlgorithms
+{
+    class Program
+    {
+        //Check if a string is uppercase
+        static bool IsUppercase(string name)
+        {
+            return name.All(char.IsUpper);
+        }
+
+        //Check if a string is lowercase
+        static bool IsLowercase(string name)
+        {
+            return name.All(char.IsLower);
+        }
+
+        //Passowrd validtor
+        //Checks is password has at least one lowercase letter, one uppercase letter and one digit
+        static bool IsPasswordComplex(string name)
+        {
+            return name.Any(char.IsDigit) && name.Any(char.IsUpper) && name.Any(char.IsLower);
+        }
+        static void Main(string[] args)
+        {
+            //Test IsUppercase and IsLowercase methods
+            Console.WriteLine(IsUppercase("Hello")); //Expected answer: False
+            Console.WriteLine(IsUppercase("HELLO")); //Expected answer: True
+            Console.WriteLine();
+
+            Console.WriteLine(IsLowercase("Hello")); //Expected answer: False
+            Console.WriteLine(IsLowercase("Hello")); //Expected answer: False
+            Console.WriteLine();
+
+            //Test IsPasswordComplex method
+            Console.WriteLine(IsPasswordComplex("ValidateString")); //Expected answer: False
+            Console.WriteLine(IsPasswordComplex("ValidateString2")); //Expected answer: True
+        }
+    }
+}
