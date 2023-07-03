865a0bc02f2d6b5ab024d96e33ea323116c7c50a
IamEnoch <enoch.mwangi@outlook.com>
2023-07-03 09:16:10 +0300

    Algorithm to carry out binary search

---
+++ String Algorithms/BinarySearch/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ArrayAlgorithms
+{
+    class Program
+    {
+        /// <summary>
+        /// Method to perform binary search
+        /// </summary>
+        /// <param name="array">Input array(Sorted and ascending)</param>
+        /// <param name="item">Specific item</param>
+        /// <returns></returns>
+        static Boolean BinarySearch(int[] array, int item)
+        {
+            //Variable to store the min and max and mid
+            int min = 0;
+            int max = array.Length - 1;
+
+            //Search completes when max is less than min
+            while (min <= max)
+            {
+                //calculate mid
+                int mid = (min + max) / 2;
+                if (array[mid] == item)
+                {
+                    return true;
+                
+                }else if (item > array[mid])
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 3, 7, 9, 11, 20 };
+            //Find if number 11 exists
+            Console.WriteLine(BinarySearch(myArray, 11)); //Expected output: True
+
+
+            //Find if number 30 exists
+            Console.WriteLine(BinarySearch(myArray, 30)); //Expected output: False
+
+        }
+    }
+}
