f6053b15294c3608117798dc0eb9689c94abf207
Tech Wiz <enoch.mwangi@outlook.com>
2023-07-20 11:15:23 +0300

    Printing the list with newly added items

--- Linked List/CommonOperations/Program.cs
+++ Linked List/CommonOperations/Program.cs
@@ -16,11 +16,26 @@ class Program
         myLinkedList.AddLast("Sarah");
 
         //Iterate through the linked list to print the elements in the linked list
+        Console.WriteLine("Printing the linked list elements");
         foreach (string item in myLinkedList)
         {
-            Console.Write(item + "=>"); //Expected output John=>Doe=>Sarah
-            Console.WriteLine();
+            Console.Write(item + "=>"); //Expected output John=>Doe=>Sarah=>
         }
+        Console.WriteLine();
+
+        //Adding elements from the head
+        myLinkedList.AddFirst("Hibiscus");
+        myLinkedList.AddFirst("Orange");
+
+        Console.WriteLine();
+
+        //Iterate through the linked list to print the elements in the linked list
+        Console.WriteLine("Printing the linked list elements");
+        foreach (string item in myLinkedList)
+        {
+            Console.Write(item + "=>"); //Expected output Orange=>Hibiscus=>John=>Doe=>Sarah=>
+        }
+        Console.WriteLine();
 
     }
 }
