28632dbdfda1d81f9977da39c417e769c202b00c
Tech Wiz <enoch.mwangi@outlook.com>
2023-07-20 10:31:37 +0300

    Adding elements from the tail

--- Linked List/CommonOperations/Program.cs
+++ Linked List/CommonOperations/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+namespace CommonOperations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Create the linked list
+        LinkedList<string> myLinkedList = new LinkedList<string>();
+
+        //Add elements to the linked list
+        //Adding elements from the tail
+        myLinkedList.AddLast("John");
+        myLinkedList.AddLast("Doe");
+        myLinkedList.AddLast("Sarah");
+
+        //Iterate through the linked list to print the elements in the linked list
+        foreach (string item in myLinkedList)
+        {
+            Console.Write(item + "=>"); //Expected output John=>Doe=>Sarah
+            Console.WriteLine();
+        }
+
+    }
+}
