a077a937ca01b6fef838ebcdff1d22290050a437
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-10-22 20:14:15 +0300

    Refactor: Update singly linked list

--- DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -3,20 +3,22 @@ namespace SinglyLinkedList;
 public class SinglyLinkedList<T>
 {
     //Head of the linked list
-    private Node<T>? _head = null;
+    private SinglyLinkedListNode<T>? _head = null;
 
     //Add first node
     public void AddFirst(T data)
     {
-        var newNode = new Node<T>(data);
-        newNode.Next = _head;
+        var newNode = new SinglyLinkedListNode<T>(data)
+        {
+            Next = _head
+        };
         _head = newNode;
     }
 
     // Add last node
     public void AddLast(T data)
     {
-        var newNode = new Node<T>(data);
+        var newNode = new SinglyLinkedListNode<T>(data);
 
         if (_head == null)
         {
@@ -24,8 +26,8 @@ public class SinglyLinkedList<T>
         }
         else
         {
-            Node<T> current = _head;
-            while (current.Next != null) current = current.Next;
+            var current = _head;
+            while (current.Next != null) current = current.Next; // Forward traversal of the singly linked list
 
             current.Next = newNode;
         }
---
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedListNode.cs
@@ -0,0 +1,7 @@
+namespace SinglyLinkedList;
+
+public class SinglyLinkedListNode<T>(T data)
+{
+    public T Data { get; } = data;
+    public SinglyLinkedListNode<T>? Next { get; set; } = null;
+}
